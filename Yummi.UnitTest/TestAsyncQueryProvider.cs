4b8fde4a402ce683030c735269e0e6b567e4acbd
romanticHate <aivp032381@gmail.com>
2023-12-28 17:53:35 -0700

    UniTest, code refactor , mock

---
+++ Yummi.UnitTest/TestAsyncQueryProvider.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Yummi.UnitTest
+{
+    public class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        public TestAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestAsyncEnumerable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return _inner.Execute(expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return _inner.Execute<TResult>(expression);
+        }
+
+        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TResult>(expression);
+        }
+
+        public ValueTask<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return new ValueTask<TResult>(_inner.Execute<TResult>(expression));
+        }
+
+        TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Yummi.UnitTest/UT_GenericRepository.cs
+++ Yummi.UnitTest/UT_GenericRepository.cs
@@ -13,21 +13,28 @@ namespace Yummi.UnitTest
         public async void From_Recipe_GetAllRecipes()
         {
             // Arrange
-
             var mockRecipeList = new List<Recipe>
             {
                 new Recipe { Id = 1, Name = "spaghetti bolognese" },
-                new Recipe { Id = 3, Name = "TEST" }
-            };
+                new Recipe { Id = 3, Name = "TEST" },
+                new Recipe { Id = 5, Name = "arroz chino" }
+            }.AsQueryable();
 
             var mockDbSet = new Mock<DbSet<Recipe>>();
-            mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.Provider).Returns(mockRecipeList.AsQueryable().Provider);
-            mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(mockRecipeList.AsQueryable().Expression);
-            mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(mockRecipeList.AsQueryable().ElementType);
+            mockDbSet.As<IAsyncEnumerable<Recipe>>()
+                .Setup(m => m.GetAsyncEnumerator(new CancellationToken()))
+                .Returns(new TestAsyncEnumerator<Recipe>(mockRecipeList.GetEnumerator()));
+
+            mockDbSet.As<IQueryable<Recipe>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestAsyncQueryProvider<Recipe>(mockRecipeList.Provider));
+
+            mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.Expression).Returns(mockRecipeList.Expression);
+            mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.ElementType).Returns(mockRecipeList.ElementType);
             mockDbSet.As<IQueryable<Recipe>>().Setup(m => m.GetEnumerator()).Returns(() => mockRecipeList.GetEnumerator());
 
             var mockContext = new Mock<YummiDbContext>();
-            mockContext.Setup(m => m.Set<Recipe>()).Returns(mockDbSet.Object);
+            mockContext.Setup(c => c.Set<Recipe>()).Returns(mockDbSet.Object);
 
             var repository = new GenericRepository<Recipe>(mockContext.Object);
 
@@ -36,8 +43,8 @@ namespace Yummi.UnitTest
 
             // Assert
             Assert.NotNull(recipes);
-            Assert.Equal(2, recipes.Count());           
-            Assert.Equal("TEST", recipes.ElementAt(1).Name);
+            Assert.Equal(3, recipes.Count());           
+            Assert.Equal("arroz chino", recipes.ElementAt(0).Name);
         }
        
 
